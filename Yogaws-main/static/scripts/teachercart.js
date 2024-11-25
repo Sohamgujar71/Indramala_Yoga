3402118f0c8e1a6bc8db83c80ad84f686a3043e4
erratus <115651961+erratus@users.noreply.github.com>
2024-11-25 17:19:47 +0530

    able to change values based on online or offline modes

--- Yogaws-main/static/scripts/teachercart.js
+++ Yogaws-main/static/scripts/teachercart.js
@@ -1,115 +1,38 @@
 const product = [
     {
         id: 0,
-        image: "/static/assets/meditation.webp",
-        title: 'Meditation',
-        onlinePrice: 120,
-        offlinePrice: 100,
-        description: 'A practice of focusing the mind and achieving a state of mental clarity and relaxation.'
+        image: "/static/assets/200.webp",
+        title: 'YCB 200 hours',
+        onlinePrice: 14999,
+        offlinePrice: 19999,
     },
     {
         id: 1,
-        image: '/static/assets/iyengaryoga.webp',
-        title: 'Iyenger Yoga',
-        onlinePrice: 60,
-        offlinePrice: 50,
-        description: 'A style of yoga that emphasizes precise alignment and the use of props to aid in achieving postures.'
+        image: '/static/assets/300.webp',
+        title: 'YCB 300 hours',
+        onlinePrice: 19999,
+        offlinePrice: 29999,
     },
     {
         id: 2,
-        image: '/static/assets/advancedyoga.webp',
-        title: 'Advanced Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'Yoga practice involving complex postures and techniques for experienced practitioners.'
+        image: '/static/assets/500.webp',
+        title: 'YCB 300 hours',
+        onlinePrice: 29999,
+        offlinePrice: 39999,
     },
     {
         id: 3,
-        image: '/static/assets/ashtangayoga.webp',
-        title: 'Ashtanga Yoga',
-        onlinePrice: 100,
-        offlinePrice: 80,
-        description: 'A dynamic style of yoga consisting of a set sequence of postures performed in a specific order with a focus on breath and movement.'
+        image: '/static/assets/700.webp',
+        title: 'YCB 700 hours',
+        onlinePrice: 39999,
+        offlinePrice: 49999,
     },
     {
         id: 4,
-        image: '/static/assets/competitveyoga.webp',
-        title: 'Competitive Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A practice where individuals perform yoga postures and sequences to compete in events based on technique and form.'
-    },
-    {
-        id: 5,
-        image: '/static/assets/poweryoga.webp',
-        title: 'Power Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: ' A vigorous and fitness-oriented form of yoga that focuses on strength, flexibility, and endurance.'
-    },
-    {
-        id: 6,
-        image: '/static/assets/photos/fifteen.png',
-        title: 'Weight Loss Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A yoga practice designed to enhance metabolism and aid in weight management through various poses and sequences.'
-    },
-    {
-        id: 7,
-        image: '/static/assets/aerialyoga.webp',
-        title: 'Aerial Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A form of yoga performed with the aid of a suspended fabric hammock to support and enhance postures.'
-    },
-    {
-        id: 8,
-        image: '/static/assets/pregnancyyoga.webp',
-        title: 'Pregnancy Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'Yoga tailored for expectant mothers to support physical and emotional well-being during pregnancy.'
-    },
-    {
-        id: 9,
-        image: '/static/assets/vinyasayoga.webp',
-        title: 'Vinyasa Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A style of yoga characterized by a flowing sequence of poses linked with the breath, creating a smooth and dynamic practice.'
-    },
-    {
-        id: 10,
-        image: '/static/assets/yogatherapy.webp',
-        title: 'Yoga Therapy',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A therapeutic approach to yoga that uses specific postures and techniques to address individual health concerns and promote overall well-being.'
-    },
-    {
-        id: 11,
-        image: '/static/assets/stressmanagement.webp',
-        title: 'Stress Management',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'Techniques and practices designed to reduce and manage stress, often incorporating yoga, mindfulness, and relaxation methods.'
-    },
-    {
-        id: 12,
-        image: '/static/assets/kidsyoga.webp',
-        title: 'Kids Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'Yoga designed for children, focusing on playful poses, movement, and relaxation to promote physical and emotional development.'
-    },
-    {
-        id: 13,
-        image: '/static/assets/hathayoga.webp',
-        title: 'Hatha Yoga',
-        onlinePrice: 230,
-        offlinePrice: 200,
-        description: 'A comprehensive form of yoga aimed at improving strength and flexibility.'
+        image: '/static/assets/900.webp',
+        title: 'YCB 900 hours',
+        onlinePrice: 49999,
+        offlinePrice: 59999,
     }
 ];
 
@@ -139,14 +62,6 @@ document.getElementById('root').innerHTML = categories.map((item) => {
 categories.forEach((item, index) => {
     const productDiv = document.getElementById(`product-${index}`);
     const descriptionBox = document.getElementById(`description-${index}`);
-
-    const description = item.description;
-
-    productDiv.addEventListener('mouseenter', function () {
-        descriptionBox.innerHTML = `<div class="description-content">${description}</div>`;
-        descriptionBox.style.height = '100px';
-    });
-
     productDiv.addEventListener('mouseleave', function () {
         descriptionBox.style.height = '0';
         descriptionBox.innerHTML = '';
